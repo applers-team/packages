bb74a362890c8aef0b1667e0a9018093920e58a9
Can Pekesen <can@pekesen.de>
2023-07-28 12:05:49 +0200

    feat(magic-link): add possibility to provide proxyPath (e.g. for magic link generation)

--- packages/auth-magic-link-nest/src/environment.types.ts
+++ packages/auth-magic-link-nest/src/environment.types.ts
@@ -13,6 +13,7 @@ interface OAuthTokenConfig {
 }
 
 export interface AuthMagicLinkPathsConfig {
+  proxy?: string;
   magicLink: {
     request: string;
     validate: string;
--- packages/auth-magic-link-nest/src/magic-link/magic-link.strategy.ts
+++ packages/auth-magic-link-nest/src/magic-link/magic-link.strategy.ts
@@ -1,42 +1,46 @@
-import { Injectable } from "@nestjs/common";
-import { PassportStrategy } from "@nestjs/passport";
-import { Strategy } from "passport-magic-link";
-import { AuthMagicLinkUtil } from "./export.types";
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-magic-link';
+import { AuthMagicLinkUtil } from './export.types';
 import {
   InjectAuthMagicLinkConfig,
   InjectAuthMagicLinkUtil,
-} from "../module.util";
-import { MagicLinkUser } from "./types";
-import { FullAuthMagicLinkConfig } from "../types";
+} from '../module.util';
+import { MagicLinkUser } from './types';
+import { FullAuthMagicLinkConfig } from '../types';
+import URI from 'urijs';
 
 // this strategy is executed when calling the "VerifyMagicLinkGuard"
 @Injectable()
 export class MagicLinkStrategy extends PassportStrategy(Strategy) {
   constructor(
     @InjectAuthMagicLinkConfig() private config: FullAuthMagicLinkConfig,
-    @InjectAuthMagicLinkUtil() private util: AuthMagicLinkUtil
+    @InjectAuthMagicLinkUtil() private util: AuthMagicLinkUtil,
   ) {
     const { magicLinkToken } = config;
     super(
       {
         secret: magicLinkToken.secret,
-        userFields: ["email"],
-        tokenField: "token",
+        userFields: ['email'],
+        tokenField: 'token',
         ttl: magicLinkToken.ttl,
       },
       async (user: MagicLinkUser, token: string): Promise<void> => {
-        this.util.sendMagicLink(
-          token,
-          user,
-          (frontendUrl) =>
-            `${frontendUrl}${this.config.paths.magicLink.validate}?token=${token}`
+        this.util.sendMagicLink(token, user, (frontendUrl) =>
+          URI(frontendUrl)
+            .segment([
+              this.config.paths.proxy ?? '',
+              this.config.paths.magicLink.validate,
+            ])
+            .query({ token })
+            .toString(),
         );
       },
       // this is the first function which is executed
       // the return value is provided to the function above
       (user: MagicLinkUser): MagicLinkUser => {
         return { ...user, email: user.email.toLowerCase() };
-      }
+      },
     );
   }
 }
