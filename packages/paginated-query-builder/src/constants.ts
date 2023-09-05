c84af8c3f08201349736da89092bba19f47864eb
Can Pekesen <can@pekesen.de>
2023-09-05 15:57:08 +0200

    fix(paginated-query-builder): wrong default key for search

--- packages/paginated-query-builder/src/constants.ts
+++ packages/paginated-query-builder/src/constants.ts
@@ -11,4 +11,4 @@ export const DefaultSortingKeyInUrl = 'sort';
 
 export const DefaultFilterKeyInUrl = 'filter';
 
-export const DefaultSearchKeyInUrl = 'sort';
+export const DefaultSearchKeyInUrl = 'search';
